6031e61a4845e61fc771c832b5bcda282244795f
Rahul Yadav <ry.rahulmyadav@gmail.com>
2024-09-13 01:42:15 +0530

    fix: vercel redirection

--- src/pages/Enrol.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { ENROLL_FORM_URL } from '../constants/urls';
-
-export default function Enrol() {
-  useEffect(() => {
-    window.location.replace(ENROLL_FORM_URL);
-  }, []);
-  return <></>;
-}
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Enrol from './pages/Enrol';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  },
-  {
-    path: '/enrol',
-    element: <Enrol />,
   },
 ]);
