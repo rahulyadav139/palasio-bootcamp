5b4eb632c87761b6d3dd6a1d7b3a3603a55a178a
Rahul Yadav <ry.rahulmyadav@gmail.com>
2024-09-13 19:41:10 +0530

    feat: replaced typeform with google form

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const ENROL_FORM_URL = 'https://5ssgu01qj91.typeform.com/to/Vjgiwyjf';
+export const ENROL_FORM_URL = 'https://forms.gle/sVJfEfMzeJcFvXLLA';
 export const WHATSAPP_URL = 'https://wa.me/919617135659';
